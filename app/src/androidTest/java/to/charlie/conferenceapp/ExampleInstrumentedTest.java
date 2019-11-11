10619785602201d02ba21bd23e04b8dc6a953628
Charlie Robinson <charlierobinson080@gmail.com>
2019-11-11 15:41:41 +0000

    Test failing instrumented test

--- app/src/androidTest/java/to/charlie/conferenceapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/to/charlie/conferenceapp/ExampleInstrumentedTest.java
@@ -2,13 +2,13 @@ package to.charlie.conferenceapp;
 
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -24,6 +24,6 @@ public class ExampleInstrumentedTest
 		// Context of the app under test.
 		Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-		assertEquals("to.charlie.conferenceapp", appContext.getPackageName());
+		assertEquals("to.charlie.conference", appContext.getPackageName());
 	}
 }
