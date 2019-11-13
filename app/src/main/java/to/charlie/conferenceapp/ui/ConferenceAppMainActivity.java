d3cf0d58fcbbcd1f9d9593a80cbc1899ff3f8b30
Charlie Robinson <charlierobinson080@gmail.com>
2019-11-13 03:08:45 +0000

    Add hamburger menu

--- app/src/main/java/to/charlie/conferenceapp/ui/ConferenceAppMainActivity.java
+++ app/src/main/java/to/charlie/conferenceapp/ui/ConferenceAppMainActivity.java
@@ -1,16 +1,21 @@
 package to.charlie.conferenceapp.ui;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
 import to.charlie.conferenceapp.R;
@@ -18,7 +23,7 @@ import to.charlie.conferenceapp.ui.favourites.FavouritesFragment;
 import to.charlie.conferenceapp.ui.speakers.SpeakersFragment;
 import to.charlie.conferenceapp.ui.timetable.TimetableFragment;
 
-public class ConferenceAppMainActivity extends AppCompatActivity
+public class ConferenceAppMainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
 {
 	private Toolbar toolbar;
 
@@ -31,6 +36,21 @@ public class ConferenceAppMainActivity extends AppCompatActivity
 		toolbar = findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 
+		// Get the navigation drawer
+		DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
+		// Create ActionBar toggle (burger icon) for the navigation drawer
+		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.nav_open_drawer, R.string.nav_close_drawer);
+
+		drawer.addDrawerListener(toggle);
+		toggle.syncState(); // sync the state of the icon with drawer state
+
+		// Find the navigation drawer
+		NavigationView navigationView = findViewById(R.id.nav_view);
+
+		// Handle navigation drawer option selections
+		navigationView.setNavigationItemSelectedListener(this);
+
 		SectionsPagerAdapter pagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
 		ViewPager pager = findViewById(R.id.pager);
 		pager.setAdapter(pagerAdapter);
@@ -39,6 +59,20 @@ public class ConferenceAppMainActivity extends AppCompatActivity
 		tabLayout.setupWithViewPager(pager);
 
 		pager.addOnPageChangeListener(addPageChangeListener());
+	}
+
+	/**
+	 * Called when an item in the navigation drawer is selected
+	 *
+	 * @param item The item that was selected
+	 * @return true if te item was handled
+	 */
+	@Override
+	public boolean onNavigationItemSelected(@NonNull MenuItem item)
+	{
+
+		Toast.makeText(this, "Item id: " + item.getItemId(), Toast.LENGTH_LONG).show();
+		return true;
 	}
 
 	private ViewPager.OnPageChangeListener addPageChangeListener()
