3c65169dfd6b01470d21325c631be095ad2598d6
Charlie Robinson <charlierobinson080@gmail.com>
2019-11-24 23:14:04 +0000

    session view page complete

--- app/src/main/java/to/charlie/conferenceapp/ui/sessions/SessionItemFragment.java
+++ app/src/main/java/to/charlie/conferenceapp/ui/sessions/SessionItemFragment.java
@@ -1,10 +1,15 @@
 package to.charlie.conferenceapp.ui.sessions;
 
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,6 +18,15 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDate;
 import java.time.format.TextStyle;
 import java.util.Locale;
@@ -25,10 +39,19 @@ import to.charlie.conferenceapp.model.Speaker;
 
 import static androidx.core.util.Preconditions.checkArgument;
 
-public class SessionItemFragment extends Fragment
+public class SessionItemFragment extends Fragment implements OnMapReadyCallback
 {
-	TextView speakerName;
+	private TextView speakerName;
+	private TextView speakerBiography;
+	private ImageView speakerImage;
 
+	private TextView locationNameHeader;
+	private TextView locationName;
+	private TextView locationDescription;
+
+	private MapView locationMap;
+
+	private Location location;
 
 	@Nullable
 	@Override
@@ -39,12 +62,20 @@ public class SessionItemFragment extends Fragment
 		SessionViewModel sessionViewModel = ViewModelProviders.of(this).get(SessionViewModel.class);
 
 		TextView sessionTitle = view.findViewById(R.id.session_view_title);
-		TextView sessionDate = view.findViewById(R.id.session_view_date);
-		TextView sessionTime = view.findViewById(R.id.session_view_time);
+		TextView sessionDateTime = view.findViewById(R.id.session_view_date_time);
 		TextView sessionDescription = view.findViewById(R.id.session_view_session_description);
 		TextView sessionType = view.findViewById(R.id.session_view_session_type);
 
 		speakerName = view.findViewById(R.id.session_view_speaker_name);
+		speakerImage = view.findViewById(R.id.session_view_speaker_picture);
+		speakerBiography = view.findViewById(R.id.session_view_speaker_biography);
+
+		locationName = view.findViewById(R.id.session_view_location);
+		locationNameHeader = view.findViewById(R.id.session_view_location_name);
+		locationDescription = view.findViewById(R.id.session_view_location_description);
+
+		locationMap = view.findViewById(R.id.session_view_location_map);
+		locationMap.onCreate(savedInstanceState);
 
 		Bundle arguments = getArguments();
 		if (arguments != null)
@@ -54,16 +85,24 @@ public class SessionItemFragment extends Fragment
 			LocalDate sessionInLocalDate = LocalDate.parse(session.getSessionDate());
 			Locale locale = view.getResources().getConfiguration().getLocales().get(0);
 
+			//want to display the date as Wednesday 12th December 2019
 			String dayOfWeek = sessionInLocalDate.getDayOfWeek().getDisplayName(TextStyle.FULL, locale);
+			String dayOfMonth = String.valueOf(sessionInLocalDate.getDayOfMonth());
+			String daySuffix = getDayOfMonthSuffix(sessionInLocalDate.getDayOfMonth());
 			String monthName = sessionInLocalDate.getMonth().getDisplayName(TextStyle.FULL, locale);
 
+			String wholeDateString = dayOfWeek + " " + dayOfMonth + daySuffix + " " + monthName;
+
+			String dateTimeString = wholeDateString + ", " + session.getTimeStart() + " - "
+							+ session.getTimeEnd();
+
 			sessionTitle.setText(session.getTitle());
-			sessionDate.setText(session.getSessionDate());
-			sessionTime.setText(session.getTimeStart());
+			sessionDateTime.setText(dateTimeString);
 			sessionDescription.setText(session.getContent());
 			sessionType.setText(session.getSessionType().getTypeName());
 
 			LiveData<Location> locationLiveData = sessionViewModel.getLocationWithId(session.getLocationId());
+			locationLiveData.observe(this, this::setLocationInfo);
 
 			//Not every session has a speaker e.g. registration so check if there is a speaker id and
 			// if so load it and set the views
@@ -72,10 +111,6 @@ public class SessionItemFragment extends Fragment
 				LiveData<Speaker> speakerLiveData = sessionViewModel.getSpeakerWithId(session.getSpeakerId());
 				speakerLiveData.observe(this, this::setSpeakerInfo);
 			}
-//			else
-//			{
-//				speakerName.setHeight(0);
-//			}
 
 		}
 
@@ -106,6 +141,67 @@ public class SessionItemFragment extends Fragment
 
 	private void setSpeakerInfo(Speaker speaker)
 	{
-		speakerName.setText(speaker.getName());
+		String nameHeaderString =
+						getResources().getString(R.string.speaker_name_first_part) + " " + speaker.getName();
+		speakerName.setText(nameHeaderString);
+		speakerBiography.setText(speaker.getBiography());
+
+		AssetManager assetManager = getResources().getAssets();
+		try (InputStream is = assetManager.open("images/" + speaker.getId() + ".jpg"))
+		{
+			Bitmap bitmap = BitmapFactory.decodeStream(is);
+			speakerImage.setImageBitmap(bitmap);
+		} catch (IOException e)
+		{
+			Log.e("Speaker Image", "Error opening speaker image file", e);
+		}
+
+	}
+
+	private void setLocationInfo(Location location)
+	{
+		this.location = location;
+
+		locationNameHeader.setText(location.getName());
+		locationName.setText(location.getName());
+		locationDescription.setText(location.getDescription());
+
+		locationMap.getMapAsync(this);
+	}
+
+	@Override
+	public void onMapReady(GoogleMap googleMap)
+	{
+		LatLng locationMarker = new LatLng(location.getLatitude(), location.getLongitude());
+		googleMap.addMarker(new MarkerOptions().position(locationMarker).title(location.getName()));
+		googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(locationMarker, 15.0f));
+	}
+
+	@Override
+	public void onResume()
+	{
+		super.onResume();
+		locationMap.onResume();
+	}
+
+	@Override
+	public void onPause()
+	{
+		super.onPause();
+		locationMap.onPause();
+	}
+
+	@Override
+	public void onDestroy()
+	{
+		super.onDestroy();
+		locationMap.onDestroy();
+	}
+
+	@Override
+	public void onLowMemory()
+	{
+		super.onLowMemory();
+		locationMap.onLowMemory();
 	}
 }
