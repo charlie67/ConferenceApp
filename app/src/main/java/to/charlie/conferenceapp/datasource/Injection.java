48b897bf01ae8464fa0f06272e2b6f49dbf81bdb
Charlie Robinson <charlierobinson080@gmail.com>
2019-12-02 15:31:24 +0000

    comments

--- app/src/main/java/to/charlie/conferenceapp/datasource/Injection.java
+++ app/src/main/java/to/charlie/conferenceapp/datasource/Injection.java
@@ -2,6 +2,12 @@ package to.charlie.conferenceapp.datasource;
 
 import android.content.Context;
 
+/**
+ * This is only used for creating the database for tests
+ *
+ * @author Charlie Robinson
+ * @version 2/12/19
+ */
 public class Injection
 {
 	public static ConferenceRoomDatabase getDatabase(Context context)
